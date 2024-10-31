3dddc3c0e489ff0fe87c77fe52d45c65737ddf87
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-10-31 21:09:50 +0700

    ui/ux update

---
+++ script/ui-uxScript.js
@@ -0,0 +1,22 @@
+
+const compositionBtn = document.getElementById("comFunc")
+const inverseBtn = document.getElementById("invFunc")
+
+const  composition = document.getElementById("composition");
+const  inverse = document.getElementById("inverse");
+
+inverseBtn.addEventListener("click",function(){
+    this.classList.remove("opacity-40");
+    compositionBtn.classList.add("opacity-40");
+
+    inverse.classList.remove("hidden");
+    composition.classList.add("hidden");
+})
+
+compositionBtn.addEventListener("click",function(){
+    this.classList.remove("opacity-40");
+    inverseBtn.classList.add("opacity-40");
+
+    inverse.classList.add("hidden");
+    composition.classList.remove("hidden");
+})
